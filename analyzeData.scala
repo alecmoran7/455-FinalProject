96ca286ec2c72b73f99c52dcc3fab2a691dd6883
alecmoran7 <alec.moran7@gmail.com>
2022-04-27 16:13:49 -0600

    Final touches

--- analyzeData.scala
+++ analyzeData.scala
@@ -3,48 +3,53 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions._
 import org.apache.hadoop.fs._
-
 import org.apache.spark.ml.feature.{VectorAssembler, StringIndexer}
 import org.apache.spark.ml.regression.{RandomForestRegressor, LinearRegression}
 import org.apache.spark.ml.evaluation.RegressionEvaluator
-
+//------------------------
+// IMPORTING DATAFRAME FROM PREVIOUS JOB 
+//------------------------
 val fs = FileSystem.get(sc.hadoopConfiguration)
 val file = fs.globStatus(new Path("/fpOutput/job1/part*"))(0).getPath().toString()
 val df_imported= spark.read.options(Map("header"->"true")).csv(file)
 val df_main= df_imported.selectExpr("STATE","COUNTY","cast(2010_AVERAGE_HOUSEHOLD_INCOME as double) 2010_AVERAGE_HOUSEHOLD_INCOME","cast(RATIO_NO_HS_DIPLOMA as double) RATIO_NO_HS_DIPLOMA","cast(SVI_SCORE_SOCIOECONOMIC as double) SVI_SCORE_SOCIOECONOMIC","cast(AVERAGE_UP_SPEED as double) AVERAGE_UP_SPEED","cast(AVERAGE_DL_SPEED as double) AVERAGE_DL_SPEED")
-df_main.printSchema()
-  //------------------------
-
-// Computing covariance
-print("Covariance between internet download speed and the number of high school dropouts per county:" + df_main.stat.cov("AVERAGE_DL_SPEED", "RATIO_NO_HS_DIPLOMA"))
-print("Covariance between internet upload speed and the number of high school dropouts per county:" + df_main.stat.cov("AVERAGE_UP_SPEED", "RATIO_NO_HS_DIPLOMA"))
-print("Covariance between number of high school dropouts and socioeconomic vulnerability levels per county:" + df_main.stat.cov("RATIO_NO_HS_DIPLOMA", "SVI_SCORE_SOCIOECONOMIC"))
-print("Covariance between number of high school dropouts and household income levels per county:" + df_main.stat.cov("RATIO_NO_HS_DIPLOMA", "2010_AVERAGE_HOUSEHOLD_INCOME"))
-print("Covariance between internet download speed and socioeconomic vulnerability per county:" + df_main.stat.cov("AVERAGE_DL_SPEED", "SVI_SCORE_SOCIOECONOMIC"))
-print("Covariance between internet upload speed and socioeconomic vulnerability per county:" + df_main.stat.cov("AVERAGE_UP_SPEED", "SVI_SCORE_SOCIOECONOMIC"))
+println("Importing csv from job1, schema found: " + df_main.printSchema())
 //------------------------
-
-// Computing correlation coefficient
-print("[MAIN INTEREST]: Correlation between internet download speed and the number of high school dropouts per county:" + df_main.stat.corr("AVERAGE_DL_SPEED", "RATIO_NO_HS_DIPLOMA"))
-print("[MAIN INTEREST]: Correlation between internet upload speed and the number of high school dropouts per county:" + df_main.stat.corr("AVERAGE_UP_SPEED", "RATIO_NO_HS_DIPLOMA"))
-print("Correlation between number of high school dropouts and socioeconomic vulnerability levels per county:" + df_main.stat.corr("RATIO_NO_HS_DIPLOMA", "SVI_SCORE_SOCIOECONOMIC"))
-print("Correlation between number of high school dropouts and household income levels per county:" + df_main.stat.corr("RATIO_NO_HS_DIPLOMA", "2010_AVERAGE_HOUSEHOLD_INCOME"))
-print("Correlation between internet download speed and socioeconomic vulnerability per county:" + df_main.stat.corr("AVERAGE_DL_SPEED", "SVI_SCORE_SOCIOECONOMIC"))
-print("Correlation between internet upload speed and socioeconomic vulnerability per county:" + df_main.stat.corr("AVERAGE_UP_SPEED", "SVI_SCORE_SOCIOECONOMIC"))
+// COMPUTING COVARIANCE
 //------------------------
-
+//print("Covariance between internet download speed and the number of high school dropouts per county:" + df_main.stat.cov("AVERAGE_DL_SPEED", "RATIO_NO_HS_DIPLOMA"))
+val cov_1: (String, Double) = ("Covariance between internet download speed and the number of high school dropouts per county", df_main.stat.cov("AVERAGE_DL_SPEED", "RATIO_NO_HS_DIPLOMA"))
+val cov_2: (String, Double) = ("Covariance between internet upload speed and the number of high school dropouts per county:", df_main.stat.cov("AVERAGE_UP_SPEED", "RATIO_NO_HS_DIPLOMA"))
+val cov_3: (String, Double) = ("Covariance between number of high school dropouts and socioeconomic vulnerability levels per county:", df_main.stat.cov("RATIO_NO_HS_DIPLOMA", "SVI_SCORE_SOCIOECONOMIC"))
+val cov_4: (String, Double) = ("Covariance between number of high school dropouts and household income levels per county:", df_main.stat.cov("RATIO_NO_HS_DIPLOMA", "2010_AVERAGE_HOUSEHOLD_INCOME"))
+val cov_5: (String, Double) = ("Covariance between internet download speed and socioeconomic vulnerability per county:", df_main.stat.cov("AVERAGE_DL_SPEED", "SVI_SCORE_SOCIOECONOMIC"))
+val cov_6: (String, Double) = ("Covariance between internet upload speed and socioeconomic vulnerability per county:", df_main.stat.cov("AVERAGE_UP_SPEED", "SVI_SCORE_SOCIOECONOMIC"))
+val covStats = List(cov_1,cov_2,cov_3,cov_4,cov_5,cov_6)
+//------------------------
+// COMPUTING THE CORRELATION COEFFICIENT 
+//------------------------
+val corr_1: (String, Double) = ("[MAIN INTEREST]: Correlation between internet download speed and the number of high school dropouts per county:", df_main.stat.corr("AVERAGE_DL_SPEED", "RATIO_NO_HS_DIPLOMA"))
+val corr_2: (String, Double) = ("[MAIN INTEREST]: Correlation between internet upload speed and the number of high school dropouts per county:", df_main.stat.corr("AVERAGE_UP_SPEED", "RATIO_NO_HS_DIPLOMA"))
+val corr_3: (String, Double) = ("Correlation between number of high school dropouts and socioeconomic vulnerability levels per county:", df_main.stat.corr("RATIO_NO_HS_DIPLOMA", "SVI_SCORE_SOCIOECONOMIC"))
+val corr_4: (String, Double) = ("Correlation between number of high school dropouts and household income levels per county:", df_main.stat.corr("RATIO_NO_HS_DIPLOMA", "2010_AVERAGE_HOUSEHOLD_INCOME"))
+val corr_5: (String, Double) = ("Correlation between internet download speed and socioeconomic vulnerability per county:", df_main.stat.corr("AVERAGE_DL_SPEED", "SVI_SCORE_SOCIOECONOMIC"))
+val corr_6: (String, Double) = ("Correlation between internet upload speed and socioeconomic vulnerability per county:", df_main.stat.corr("AVERAGE_UP_SPEED", "SVI_SCORE_SOCIOECONOMIC"))
+val corrStats = List(corr_1,corr_2,corr_3,corr_4,corr_5,corr_6)
+//------------------------
+// TRANSFORMING DATAFRAME INTO LABEL & FEATURE DATAFRAME FOR MACHINE LEARNING
+//------------------------
 var assembler = new VectorAssembler().setInputCols(Array("2010_AVERAGE_HOUSEHOLD_INCOME","SVI_SCORE_SOCIOECONOMIC","AVERAGE_UP_SPEED","AVERAGE_DL_SPEED")).setOutputCol("features")
 var df = assembler.transform(df_main)
-
 val df_1 = df.drop("STATE")                                                                                                                                                  
 val df_2 = df_1.drop("COUNTY")                                                                                                                                               
 val df_3 = df_2.drop("2010_AVERAGE_HOUSEHOLD_INCOME")                                                                                                      
 val df_4 = df_3.drop("SVI_SCORE_SOCIOECONOMIC")                                                                                                                              
 val df_5 = df_4.drop("AVERAGE_UP_SPEED")                                                                                                                                     
 val df_6 = df_5.drop("AVERAGE_DL_SPEED")
-
 val df_7 = df_6.withColumnRenamed("RATIO_NO_HS_DIPLOMA","label")
-
+//------------------------
+// COMPUTING LINEAR REGRESSION, RMSE, MSE, R^2, AND RESIDUAL DATA
+//------------------------
 var lr = new LinearRegression()
 var Array(train, test) = df_7.randomSplit(Array(.8,.2), 42)
 var lrModel = lr.fit(train)
@@ -58,13 +63,16 @@ re.setMetricName("r2")
 val r2 = re.evaluate(lrPredictions)
 re.setMetricName("mae")
 val mae = re.evaluate(lrPredictions)
-
-// Summarize the model over training set
 val trainingSummary = lrModel.summary
-println(s"numIterations: ${trainingSummary.totalIterations}")
-println(s"objectiveHistory: [${trainingSummary.objectiveHistory.mkString(",")}]")
+//------------------------
+// SUMMARY OF DATA 
+//------------------------
+for (c1 <- covStats) println(c1)
+for (c2 <- corrStats) println(c2)
 trainingSummary.residuals.show()
+println(s"MAE: ${mae}")
+println(s"MSE: ${mse}")
 println(s"RMSE: ${trainingSummary.rootMeanSquaredError}")
 println(s"r2: ${trainingSummary.r2}")
-
-
+//------------------------
+// END OF COMPUTATIONS 
--- joinData.scala
+++ joinData.scala
@@ -1,16 +1,17 @@
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions._
-
+//------------------------
 // IMPORTING THE DATASETS
+//------------------------
 val df_householdIncome = spark.read.options(Map("header"->"true")).csv("/fpDatasets/householdIncome.csv")
 val df_dropout = spark.read.options(Map("header"->"true")).csv("/fpDatasets/svi_dropout.csv")
 val df_internet = spark.read.options(Map("header"->"true")).csv("/fpDatasets/internetData.csv")
 val df_codes = spark.read.options(Map("header"->"true")).csv("/fpDatasets/US_FIPS_Codes.csv")
 val df_svi= spark.read.options(Map("header"->"true")).csv("/fpDatasets/se_svi.csv")
 //------------------------
-
 // REDUCING THE DATASETS
+//------------------------
 val internet_part_1 = df_internet.groupBy("STATE", "STATE_NUMBER", "COUNTY_NUMBER").agg(mean("MaxAdDown").alias("AVERAGE_DL_SPEED"))
 val internet_part_2 = df_internet.groupBy("STATE", "STATE_NUMBER", "COUNTY_NUMBER").agg(mean("MaxAdUp").alias("AVERAGE_UP_SPEED"))
 val df_internet_reduced = internet_part_2.join(internet_part_1, Seq("STATE", "STATE_NUMBER", "COUNTY_NUMBER"))
@@ -18,15 +19,21 @@ val df_dropout_reduced = df_dropout.groupBy("STATE","COUNTY").agg(mean("MP_NOHSD
 val df_income_reduced = df_householdIncome.groupBy("STATE","COUNTY").agg(mean("2010_MEDIAN_HOUSEHOLD_INCOME").alias("2010_AVERAGE_HOUSEHOLD_INCOME"))
 val df_svi_reduced = df_svi.groupBy("STATE","COUNTY").agg(mean("RPL_THEME1").alias("SVI_SCORE_SOCIOECONOMIC"))
 //---------------------
-
 // JOINING THE DATASETS
+//---------------------
 val ft_1 = df_income_reduced.join(df_codes.withColumnRenamed("COUNTY NAME","COUNTY"), Seq("STATE","COUNTY"))
 val ft_2 = ft_1.join(df_dropout_reduced, Seq("STATE", "COUNTY"))
 val ft_3 = ft_2.join(df_svi_reduced, Seq("STATE", "COUNTY"))
 val ft_4 = ft_3.join(df_internet_reduced.withColumnRenamed("STATE","STATE_ABBREV"), ft_3("FIPS STATE") === df_internet_reduced("STATE_NUMBER") && ft_3("FIPS COUNTY") === df_internet_reduced("COUNTY_NUMBER"), "outer")
 val ft_5 = ft_4.drop("FIPS STATE").drop("FIPS COUNTY").drop("STATE_NUMBER").drop("COUNTY_NUMBER").drop("STATE_ABBREV")
 val ft_6 = ft_5.na.drop("any")
+//---------------------
+// PREVIEW OF GENERATED DATAFRAME 
+//---------------------
 ft_6.show(true)
+//---------------------
+// WRITING DATAFRAME TO FILE AT DIRECTORY: hdfs://fpOutput/job1
+//---------------------
 ft_6.write.option("header",true).csv("/fpOutput/job1/")
-
-
+//---------------------
+// END OF JOB 1, FOR JOB 2 USE analyzeData.scala 
